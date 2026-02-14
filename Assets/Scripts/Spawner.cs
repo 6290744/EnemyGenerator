af57a681b9c281b9e21e958c56c059fe0b213a7e
ERisunkov <6290744@gmail.com>
2026-02-14 22:37:01 +0100

    Spawner created

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+using Random = UnityEngine.Random;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private List<SpawnPoint> _spawnPoints;
+    [SerializeField] private Enemy _enemyPrefab;
+    [SerializeField] private float _spawnInterval;
+    [SerializeField] private int _enemyMaximalCount;
+    [SerializeField] private int _enemyCapacity;
+ 
+    private ObjectPool<Enemy> _poolOfEnemies;
+
+     private void Awake()
+     {
+         _poolOfEnemies = new ObjectPool<Enemy>(
+             createFunc: () => Instantiate(_enemyPrefab, ChooseStartPosition(), ChooseStartRotation()),
+             actionOnGet: (enemy) => OnGetFromPool(enemy),
+             actionOnRelease: (enemy) =>  OnReleaseToPool(enemy),
+             actionOnDestroy: (enemy) => Destroy(enemy.gameObject),
+             collectionCheck: true,
+             defaultCapacity: _enemyCapacity,
+             maxSize: _enemyMaximalCount
+             );
+     }
+
+     private void OnGetFromPool(Enemy enemy)
+     {
+         enemy.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+         enemy.gameObject.SetActive(true);
+         enemy.Run();
+
+         enemy.Death += OnEnemyDeath;
+     }
+
+     private void OnReleaseToPool(Enemy enemy)
+     {
+         enemy.gameObject.SetActive(false);
+         
+         enemy.Death -= OnEnemyDeath;
+     }
+
+     private void OnEnemyDeath(Enemy enemy)
+     {
+         _poolOfEnemies.Release(enemy);
+     }
+
+     private Vector3 ChooseStartPosition()
+     {
+         int maximalSpawnPointIndex = _spawnPoints.Count - 1;
+         int choosedSpawnPointIndex = Random.Range(0, maximalSpawnPointIndex);
+         
+         return _spawnPoints[choosedSpawnPointIndex].GetSpawnPosition();
+     }
+
+     private Quaternion ChooseStartRotation()
+     {
+         int rotationX = 0;
+         int rotationZ = 0;
+         int rotationY = Random.Range(0, 360);
+         
+         return Quaternion.Euler(rotationX, rotationY, rotationZ);
+     }
+}
