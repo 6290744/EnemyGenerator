31847b960eeb7aa73ba87206ab4b7cb8e5d82c42
ERisunkov <6290744@gmail.com>
2026-02-15 17:47:32 +0100

    Revert "Remove methods from instantiation"
    This reverts commit d97a7dd83dde5710fcc00e6523ea131c58b780b6.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,51 +1,28 @@
 using System;
-using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody), typeof(Collider))]
-
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed = 5f;
-
-    private bool _isMoving;
-
-    public event Action<Enemy> Death;
-
-    public void SetStartPosition(Vector3 position)
-    {
-        transform.position = position;
-    }
-
-    public void SetStartRotation(Quaternion rotation)
-    {
-        transform.rotation = rotation;
-    }
+    private Quaternion _startRotation;
     
-    private void OnEnable()
+    public event Action<Enemy> Death;
+    
+    public void SetRunDirection(Quaternion direction)
     {
-        _isMoving = true;
-
-        StartCoroutine(MoveLoop());
+        
     }
 
-    private void OnCollisionEnter(Collision collision)
+    public void Run()
     {
-        if (collision.gameObject.TryGetComponent<Wall>(out _))
-        {
-            _isMoving = false;
+        
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent<Wall>(out _))
+        {
             Death?.Invoke(this);
-        }
-    }
-
-    private IEnumerator MoveLoop()
-    {
-        while (_isMoving)
-        {
-            transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
-
-            yield return null;
         }
     }
 }
